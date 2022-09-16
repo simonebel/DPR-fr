b5b1d04554d98a8856b89d06f0df4cef6053dd95
simon <ebel.simon@hotmail.com>
2022-09-16 14:51:02 +0200

    fix: remove distribtued and add shard for gen slurm

--- gen_dense.sh
+++ gen_dense.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# SCRIPT NAME: multi-dpr.sh
-#SBATCH --job-name=dpr-stat
-#SBATCH --nodes=4
+# SCRIPT NAME: gen_dense.sh
+#SBATCH --job-name=dpr-gen
+#SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=8
+#SBATCH --cpus-per-task=32
 #SBATCH --gres=gpu:1
 #SBATCH --output=/home/cedar/sebel/DPR-fr/slurm_log/gen_dense/job_%j.out
 #SBATCH --error=/home/cedar/sebel/DPR-fr/slurm_log/gen_dense/job_%j.err
@@ -13,8 +13,8 @@ source /home/cedar/sebel/miniconda3/bin/activate dpr
 set -x
 export PYTHONPATH=/home/cedar/sebel/miniconda3/envs/dpr/bin/python
 # Launch our script
-srun python -m torch.distributed.launch --nproc_per_node=1 \
-    generate_dense_embeddings.py \
+srun python generate_dense_embeddings.py \
     model_file=/scratch/sebel/dpr-stat/dpr-ckpt/dpr_biencoder.99 \
     ctx_src=dpr_insee \
+    shard_id=0 num_shards=1 \
     out_file=/scratch/sebel/dpr-stat/dpr-emb/dpr-insee/
